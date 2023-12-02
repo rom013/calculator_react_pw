693f522f111c4111b4be42217b10c23cbcf79415
rom013 <romullomelo0130@gmail.com>
2023-12-02 11:37:45 -0300

    feat: install json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Calculator from "./components/Calculator"
+import HistoryMenu from "./components/History"
 
 function App() {
 	return (
 		<main
 			className="bg-blur min-h-screen bg-no-repeat bg-cover dark:bg-zinc-900 flex"
 		>
+			<HistoryMenu/>
 			<section
 				className="flex-1 flex items-center justify-center"
 			>
---
+++ src/components/History.jsx
@@ -0,0 +1,9 @@
+export default function HistoryMenu(){
+    return(
+        <section
+            className="absolute right-0 bg-red-500 bottom-0 top-0 px-6 py-10 w"
+        >
+            <h3>Histórico</h3>
+        </section>
+    )
+}
